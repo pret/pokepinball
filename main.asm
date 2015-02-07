bfc9d29048bbdb2fb99f4ef37bf062a447b363f2
Marcus Huderle <huderlem@gmail.com>
2015-02-06 19:58:18 -0800

    Added more billboard pics.

--- main.asm
+++ main.asm
@@ -67,7 +67,12 @@ INCBIN "bin/30000_33fff.bin" ; 0x30000
 
 SECTION "bankd", ROMX, BANK[$d]
 
-INCBIN "bin/34000_37fff.bin" ; 0x34000
+SlotOnPic: ; 0x34000
+	INCBIN "gfx/billboard/slot/slot_on.2bpp"
+SlotOffPic: ; 0x34180
+	INCBIN "gfx/billboard/slot/slot_off.2bpp"
+
+INCBIN "bin/34300_37fff.bin" ; 0x34000
 
 
 SECTION "banke", ROMX, BANK[$e]
@@ -112,7 +117,92 @@ INCBIN "bin/54000_57fff.bin" ; 0x54000
 
 SECTION "bank16", ROMX, BANK[$16]
 
-INCBIN "bin/58000_5bfff.bin" ; 0x58000
+BallSaver30SecondsOnPic: ; 0x58000
+	INCBIN "gfx/billboard/slot/30secondballsaver_on.2bpp"
+BallSaver30SecondsOffPic: ; 0x58180
+	INCBIN "gfx/billboard/slot/30secondballsaver_off.2bpp"
+BallSaver60SecondsOnPic: ; 0x58300
+	INCBIN "gfx/billboard/slot/60secondballsaver_on.2bpp"
+BallSaver60SecondsOffPic: ; 0x580480
+	INCBIN "gfx/billboard/slot/60secondballsaver_off.2bpp"
+BallSaver90SecondsOnPic: ; 0x58600
+	INCBIN "gfx/billboard/slot/90secondballsaver_on.2bpp"
+BallSaver90SecondsOffPic: ; 0x58780
+	INCBIN "gfx/billboard/slot/90secondballsaver_off.2bpp"
+PikachuSaverOnPic: ; 0x58900
+	INCBIN "gfx/billboard/slot/pikachusaver_on.2bpp"
+PikachuSaverOffPic: ; 0x58a80
+	INCBIN "gfx/billboard/slot/pikachusaver_off.2bpp"
+ExtraBallOnPic: ; 0x58c00
+	INCBIN "gfx/billboard/slot/extraball_on.2bpp"
+ExtraBallOffPic: ; 0x58d80
+	INCBIN "gfx/billboard/slot/extraball_off.2bpp"
+SmallRewardOnPic: ; 0x58f00
+	INCBIN "gfx/billboard/slot/small_on.2bpp"
+SmallRewardOffPic: ; 0x59080
+	INCBIN "gfx/billboard/slot/small_off.2bpp"
+BigRewardOnPic: ; 0x59200
+	INCBIN "gfx/billboard/slot/big_on.2bpp"
+BigRewardOffPic: ; 0x59380
+	INCBIN "gfx/billboard/slot/big_off.2bpp"
+CatchEmModeOnPic: ; 0x59500
+	INCBIN "gfx/billboard/slot/catchem_on.2bpp"
+CatchEmModeOffPic: ; 0x59680
+	INCBIN "gfx/billboard/slot/catchem_off.2bpp"
+EvolutionModeOnPic: ; 0x59800
+	INCBIN "gfx/billboard/slot/evolution_on.2bpp"
+EvolutionModeOffPic: ; 0x59980
+	INCBIN "gfx/billboard/slot/evolution_off.2bpp"
+GreatBallOnPic: ; 0x59b00
+	INCBIN "gfx/billboard/slot/greatball_on.2bpp"
+GreatBallOffPic: ; 0x59c80
+	INCBIN "gfx/billboard/slot/greatball_off.2bpp"
+UltraBallOnPic: ; 0x59e00
+	INCBIN "gfx/billboard/slot/ultraball_on.2bpp"
+UltraBallOffPic: ; 0x59f80
+	INCBIN "gfx/billboard/slot/ultraball_off.2bpp"
+MasterBallOnPic: ; 0x5a100
+	INCBIN "gfx/billboard/slot/masterball_on.2bpp"
+MasterBallOffPic: ; 0x5a280
+	INCBIN "gfx/billboard/slot/masterball_off.2bpp"
+BonusMultiplierOnPic: ; 0x5a400
+	INCBIN "gfx/billboard/slot/bonusmultiplier_on.2bpp"
+BonusMultiplierOffPic: ; 0x5a580
+	INCBIN "gfx/billboard/slot/bonusmultiplier_off.2bpp"
+HurryUpOnPic: ; 0x5a700
+	INCBIN "gfx/billboard/hurryup_on.2bpp"
+HurryUpOffPic: ; 0x5a880
+	INCBIN "gfx/billboard/hurryup_off.2bpp"
+HurryUp2OnPic: ; 0x5aa00
+	INCBIN "gfx/billboard/hurryup2_on.2bpp"
+HurryUp2OffPic: ; 0x5ab80
+	INCBIN "gfx/billboard/hurryup2_off.2bpp"
+GoToNextOnPic: ; 0x5ad00
+	INCBIN "gfx/billboard/gotonext_on.2bpp"
+GoToNextOffPic: ; 0x5ae80
+	INCBIN "gfx/billboard/gotonext_off.2bpp"
+GoToGengarBonusOnPic: ; 0x5c000
+	INCBIN "gfx/billboard/bonus_stages/gotogengarbonus_on.2bpp"
+GoToGengarBonusOffPic: ; 0x5c180
+	INCBIN "gfx/billboard/bonus_stages/gotogengarbonus_off.2bpp"
+GoToMewtwoBonusOnPic: ; 0x5c300
+	INCBIN "gfx/billboard/bonus_stages/gotomewtwobonus_on.2bpp"
+GoToMewtwoBonusOffPic: ; 0x5c480
+	INCBIN "gfx/billboard/bonus_stages/gotomewtwobonus_off.2bpp"
+GoToMeowthBonusOnPic: ; 0x5c600
+	INCBIN "gfx/billboard/bonus_stages/gotomeowthbonus_on.2bpp"
+GoToMeowthBonusOffPic: ; 0x5c780
+	INCBIN "gfx/billboard/bonus_stages/gotomeowthbonus_off.2bpp"
+GoToDiglettBonusOnPic: ; 0x5c900
+	INCBIN "gfx/billboard/bonus_stages/gotodiglettbonus_on.2bpp"
+GoToDiglettBonusOffPic: ; 0x5ca80
+	INCBIN "gfx/billboard/bonus_stages/gotodiglettbonus_off.2bpp"
+GoToSeelBonusOnPic: ; 0x5cc00
+	INCBIN "gfx/billboard/bonus_stages/gotoseelbonus_on.2bpp"
+GoToSeelBonusOffPic: ; 0x5cd80
+	INCBIN "gfx/billboard/bonus_stages/gotoseelbonus_off.2bpp"
+
+INCBIN "bin/5bf00_5bfff.bin" ; 0x5bf00
 
 
 SECTION "bank17", ROMX, BANK[$17]

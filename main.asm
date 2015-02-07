433c6764733eadb602d3ecfeccbc0cbffa58b438
Marcus Huderle <huderlem@gmail.com>
2015-02-07 12:15:50 -0800

    Added bonus multiplier reward billboard pics.

--- main.asm
+++ main.asm
@@ -734,7 +734,28 @@ PidgeyPic: ; 0x7ad00
 PidgeySilhouettePic: ; 0x7ae80
 	INCBIN "gfx/billboard/mon_silhouettes/pidgey.2bpp"
 
-INCBIN "bin/7b000_7bfff.bin" ; 0x7b000
+BonusMultiplierX1OnPic: ; 0x7b000
+	INCBIN "gfx/billboard/slot/bonusmultiplierX1_on.2bpp"
+BonusMultiplierX1OffPic: ; 0x7b180
+	INCBIN "gfx/billboard/slot/bonusmultiplierX1_off.2bpp"
+BonusMultiplierX2OnPic: ; 0x7b300
+	INCBIN "gfx/billboard/slot/bonusmultiplierX2_on.2bpp"
+BonusMultiplierX2OffPic: ; 0x7b480
+	INCBIN "gfx/billboard/slot/bonusmultiplierX2_off.2bpp"
+BonusMultiplierX3OnPic: ; 0x7b600
+	INCBIN "gfx/billboard/slot/bonusmultiplierX3_on.2bpp"
+BonusMultiplierX3OffPic: ; 0x7b780
+	INCBIN "gfx/billboard/slot/bonusmultiplierX3_off.2bpp"
+BonusMultiplierX4OnPic: ; 0x7b900
+	INCBIN "gfx/billboard/slot/bonusmultiplierX4_on.2bpp"
+BonusMultiplierX4OffPic: ; 0x7ba80
+	INCBIN "gfx/billboard/slot/bonusmultiplierX4_off.2bpp"
+BonusMultiplierX5OnPic: ; 0x7bc00
+	INCBIN "gfx/billboard/slot/bonusmultiplierX5_on.2bpp"
+BonusMultiplierX5OffPic: ; 0x7bd80
+	INCBIN "gfx/billboard/slot/bonusmultiplierX5_off.2bpp"
+
+INCBIN "bin/7bf00_7bfff.bin" ; 0x7bf00
 
 
 SECTION "bank1f", ROMX, BANK[$1f]

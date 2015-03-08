0c7e49017dc1efefbb609dded2e3a49e0b2bc8c5
Marcus Huderle <huderlem@gmail.com>
2015-03-08 11:12:26 -0700

    Evolution lines.

--- constants.asm
+++ constants.asm
@@ -1,6 +1,7 @@
 INCLUDE "hram.asm"
 INCLUDE "vram.asm"
 
+INCLUDE "constants/evolution_line_constants.asm"
 INCLUDE "constants/joy_constants.asm"
 INCLUDE "constants/map_constants.asm"
 INCLUDE "constants/pokemon_constants.asm"

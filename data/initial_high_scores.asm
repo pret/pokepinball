314760246871f739c19edbb8afa7aa6b157684ac
Marcus Huderle <huderlem@gmail.com>
2015-07-13 11:27:34 -0700

    Use charmap values for highscores names.

--- data/initial_high_scores.asm
+++ data/initial_high_scores.asm
@@ -1,21 +1,21 @@
 InitialHighScores: ; 0xd42e
 ; TODO: Use text instead of raw bytes for the names.
 	bigBCD6 50000000
-	db $4e, $49, $4e ; "NIN"
+	db "NIN"
 	db $00, $00, $00, $00
 
 	bigBCD6 40000000
-	db $43, $52, $45 ; "CRE"
+	db "CRE"
 	db $00, $00, $00, $00
 
 	bigBCD6 30000000
-	db $47, $41, $4d ; "GAM"
+	db "GAM"
 	db $00, $00, $00, $00
 
 	bigBCD6 20000000
-	db $48, $41, $4c ; "HAL"
+	db "HAL"
 	db $00, $00, $00, $00
 
 	bigBCD6 10000000
-	db $4a, $55, $50 ; "JUP"
+	db "JUP"
 	db $00, $00, $00, $00

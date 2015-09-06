dd8be3db501b7b88d9b2ed2b295c9280f2a9e24c
Marcus Huderle <huderlem@gmail.com>
2015-09-06 13:44:00 -0700

    Dump mons 71-80 billboard palette data.

--- main.asm
+++ main.asm
@@ -41599,7 +41599,181 @@ GoToSeelBonusOnPic: ; 0x5bc00
 GoToSeelBonusOffPic: ; 0x5bd80
 	INCBIN "gfx/billboard/bonus_stages/gotoseelbonus_off.2bpp"
 
-INCBIN "baserom.gbc",$5bf00,$5c000 - $5bf00 ; 0x5bf00
+ChanseyBillboardBGPalette1: ; 0x5bf00
+    RGB 31, 31, 31
+    RGB 31, 15, 20
+    RGB 27, 5, 7
+    RGB 0, 0, 0
+ChanseyBillboardBGPalette2: ; 0x5bf08
+    RGB 31, 31, 31
+    RGB 31, 15, 20
+    RGB 27, 5, 7
+    RGB 0, 0, 0
+
+TangelaBillboardBGPalette1: ; 0x5bf10
+    RGB 31, 31, 31
+    RGB 15, 21, 29
+    RGB 4, 8, 18
+    RGB 0, 0, 0
+TangelaBillboardBGPalette2: ; 0x5bf18
+    RGB 31, 31, 31
+    RGB 15, 21, 29
+    RGB 4, 8, 18
+    RGB 0, 0, 0
+
+KangaskhanBillboardBGPalette1: ; 0x5bf20
+    RGB 31, 31, 31
+    RGB 31, 18, 9
+    RGB 17, 10, 4
+    RGB 0, 0, 0
+KangaskhanBillboardBGPalette2: ; 0x5bf28
+    RGB 31, 31, 31
+    RGB 31, 18, 9
+    RGB 17, 10, 4
+    RGB 0, 0, 0
+
+HorseaBillboardBGPalette1: ; 0x5bf30
+    RGB 31, 31, 31
+    RGB 12, 19, 31
+    RGB 4, 7, 22
+    RGB 0, 0, 0
+HorseaBillboardBGPalette2: ; 0x5bf38
+    RGB 31, 31, 31
+    RGB 25, 26, 3
+    RGB 4, 7, 22
+    RGB 0, 0, 0
+
+SeadraBillboardBGPalette1: ; 0x5bf40
+    RGB 31, 31, 31
+    RGB 12, 19, 31
+    RGB 4, 7, 22
+    RGB 0, 0, 0
+SeadraBillboardBGPalette2: ; 0x5bf48
+    RGB 31, 31, 31
+    RGB 12, 19, 31
+    RGB 4, 7, 22
+    RGB 0, 0, 0
+
+GoldeenBillboardBGPalette1: ; 0x5bf50
+    RGB 31, 31, 31
+    RGB 30, 16, 4
+    RGB 29, 3, 0
+    RGB 0, 0, 0
+GoldeenBillboardBGPalette2: ; 0x5bf58
+    RGB 31, 31, 31
+    RGB 30, 16, 4
+    RGB 29, 3, 0
+    RGB 0, 0, 0
+
+SeakingBillboardBGPalette1: ; 0x5bf60
+    RGB 31, 31, 31
+    RGB 29, 17, 5
+    RGB 31, 5, 0
+    RGB 0, 0, 0
+SeakingBillboardBGPalette2: ; 0x5bf68
+    RGB 31, 31, 31
+    RGB 29, 17, 5
+    RGB 31, 5, 0
+    RGB 0, 0, 0
+
+StaryuBillboardBGPalette1: ; 0x5bf70
+    RGB 31, 31, 31
+    RGB 31, 20, 0
+    RGB 19, 3, 6
+    RGB 0, 0, 0
+StaryuBillboardBGPalette2: ; 0x5bf78
+    RGB 31, 31, 31
+    RGB 31, 16, 8
+    RGB 25, 3, 2
+    RGB 0, 0, 0
+
+StarmieBillboardBGPalette1: ; 0x5bf80
+    RGB 31, 31, 31
+    RGB 31, 17, 0
+    RGB 15, 8, 16
+    RGB 0, 0, 0
+StarmieBillboardBGPalette2: ; 0x5bf88
+    RGB 31, 31, 31
+    RGB 31, 16, 8
+    RGB 25, 3, 2
+    RGB 0, 0, 0
+
+MrMimeBillboardBGPalette1: ; 0x5bf90
+    RGB 31, 31, 31
+    RGB 31, 17, 19
+    RGB 28, 6, 4
+    RGB 0, 0, 0
+MrMimeBillboardBGPalette2: ; 0x5bf98
+    RGB 31, 31, 31
+    RGB 31, 17, 19
+    RGB 28, 6, 4
+    RGB 0, 0, 0
+
+ScytherBillboardBGPalette1: ; 0x5bfa0
+    RGB 31, 31, 31
+    RGB 17, 31, 4
+    RGB 4, 16, 4
+    RGB 0, 0, 0
+ScytherBillboardBGPalette2: ; 0x5bfa8
+    RGB 31, 31, 31
+    RGB 17, 31, 4
+    RGB 4, 16, 4
+    RGB 0, 0, 0
+
+JynxBillboardBGPalette1: ; 0x5bfb0
+    RGB 31, 31, 31
+    RGB 29, 13, 15
+    RGB 30, 6, 1
+    RGB 0, 0, 0
+JynxBillboardBGPalette2: ; 0x5bfb8
+    RGB 31, 31, 31
+    RGB 31, 21, 0
+    RGB 30, 6, 1
+    RGB 0, 0, 0
+
+ElectabuzzBillboardBGPalette1: ; 0x5bfc0
+    RGB 31, 31, 31
+    RGB 31, 27, 0
+    RGB 19, 11, 0
+    RGB 0, 0, 0
+ElectabuzzBillboardBGPalette2: ; 0x5bfc8
+    RGB 31, 31, 31
+    RGB 31, 27, 0
+    RGB 19, 11, 0
+    RGB 0, 0, 0
+
+MagmarBillboardBGPalette1: ; 0x5bfd0
+    RGB 31, 31, 31
+    RGB 31, 27, 0
+    RGB 28, 6, 0
+    RGB 0, 0, 0
+MagmarBillboardBGPalette2: ; 0x5bfd8
+    RGB 31, 31, 31
+    RGB 31, 27, 0
+    RGB 28, 6, 0
+    RGB 0, 0, 0
+
+PinsirBillboardBGPalette1: ; 0x5bfe0
+    RGB 31, 31, 31
+    RGB 17, 23, 10
+    RGB 21, 10, 3
+    RGB 0, 0, 0
+PinsirBillboardBGPalette2: ; 0x5bfe8
+    RGB 31, 31, 31
+    RGB 31, 19, 8
+    RGB 21, 10, 3
+    RGB 0, 0, 0
+
+TaurosBillboardBGPalette1: ; 0x5bff0
+    RGB 31, 31, 31
+    RGB 31, 18, 7
+    RGB 17, 9, 0
+    RGB 0, 0, 0
+TaurosBillboardBGPalette2: ; 0x5bff8
+    RGB 31, 31, 31
+    RGB 14, 16, 20
+    RGB 17, 9, 0
+    RGB 0, 0, 0
 
 
 SECTION "bank17", ROMX, BANK[$17]
@@ -43667,7 +43841,103 @@ INCBIN "baserom.gbc",$d8c40,$da000 - $d8c40
 CopyrightTextGfx: ; 0xda000
     INCBIN "gfx/copyright_text.2bpp"
 
-INCBIN "baserom.gbc",$da400,$da800 - $da400
+ChanseyBillboardBGPaletteMap: ; 0xda400
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+TangelaBillboardBGPaletteMap: ; 0xda418
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+KangaskhanBillboardBGPaletteMap: ; 0xda430
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+HorseaBillboardBGPaletteMap: ; 0xda448
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $7, $7
+
+SeadraBillboardBGPaletteMap: ; 0xda460
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+GoldeenBillboardBGPaletteMap: ; 0xda478
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+SeakingBillboardBGPaletteMap: ; 0xda490
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+StaryuBillboardBGPaletteMap: ; 0xda4a8
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $7, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+StarmieBillboardBGPaletteMap: ; 0xda4c0
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $7, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+MrMimeBillboardBGPaletteMap: ; 0xda4d8
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+ScytherBillboardBGPaletteMap: ; 0xda4f0
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+JynxBillboardBGPaletteMap: ; 0xda508
+    db $6, $7, $7, $7, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $7, $6, $6, $6
+
+ElectabuzzBillboardBGPaletteMap: ; 0xda520
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+MagmarBillboardBGPaletteMap: ; 0xda538
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+PinsirBillboardBGPaletteMap: ; 0xda550
+    db $6, $6, $6, $6, $6, $6
+    db $6, $6, $7, $6, $6, $6
+    db $6, $6, $7, $6, $6, $6
+    db $6, $6, $6, $6, $7, $7
+
+TaurosBillboardBGPaletteMap: ; 0xda568
+    db $7, $7, $6, $6, $7, $7
+    db $7, $7, $6, $7, $7, $7
+    db $6, $7, $6, $6, $6, $6
+    db $6, $6, $6, $6, $6, $6
+
+INCBIN "baserom.gbc",$da580,$da800 - $da580
 
 BulbasaurBillboardBGPaletteMap: ; 0xda800
     db $7, $7, $7, $7, $7, $7
